1fcb911eed6bd452934bed2e9000adc166f18488
Bianca Bai <110609439+BiancaBr7@users.noreply.github.com>
2023-11-24 15:41:06 -0500

    Add files via upload

---
+++ Simulation 2.0.py
@@ -0,0 +1,110 @@
+import numpy as np
+import random
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Soil:
+    def __init__(self, seed, puffball):
+        self.seed = seed
+        self.puffball = puffball
+
+column =100
+row=100
+field=[[Soil([],[]) for c in range(column)]for r in range(row)]
+
+field[0][0].puffball.append(0)
+
+def spread_seed(puff_row,puff_col):
+    global field
+    total_seed = 2000
+    survive_seed = 75/1000*total_seed
+    count=0
+    while survive_seed!=0 and count<=100:
+        random_row = random.randint(max(0,puff_row-9), min(puff_row+9,99))
+        random_col = random.randint(max(0,puff_col-9), min(puff_col+9,99))
+        if len(field[random_row][random_col].seed)<100:
+            field[random_row][random_col].seed.append(0)
+            survive_seed-=1
+            count=0
+        else:
+            count+=1
+        
+
+def heat(field,T):
+    
+    data = np.array(field)
+
+    sns.heatmap(data, cmap='viridis')
+
+    plt.title(f"Day {T}")
+
+    plt.savefig(f'Day {T}')
+
+    plt.close()
+
+
+area=[]
+
+for T in range(25):
+    area.append(0)
+    time=75
+    seed_map=[]
+    num_map=[]
+    for row in range(100):
+        seed_map.append([])
+        num_map.append([])
+        for col in range(100):
+            for puffball in field[row][col].puffball:
+                spread_seed(row,col)
+                field[row][col].puffball.remove(puffball)
+            if T<=13:
+                for seed in field[row][col].seed:
+                        if seed<75:
+                            field[row][col].seed.remove(seed)
+                            field[row][col].seed.append(seed+15)
+                        else:
+                            field[row][col].seed.remove(seed)
+                            field[row][col].puffball.append(0)
+            if len(field[row][col].seed)+len(field[row][col].puffball)!=0:
+                num_map[row].append(len(field[row][col].seed)+len(field[row][col].puffball))
+                seed_map[row].append(1)
+                
+            else:
+                seed_map[row].append(0)
+                num_map[row].append(0)
+        area[T]+=sum(seed_map[row])
+    print(f"Day is {(T+1)*15}")
+    heat(seed_map,(T+1)*15)
+    heat(num_map,f"Num_{(T+1)*15}")
+    
+print(area)
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+data = np.array(area)
+data2 = []
+
+
+
+for i in range(24):
+    if i<=14:
+        y = (((2*i/3-2/3) * 10)**2)
+        data2.append(y)
+    else:
+        data2.append((((2*14/3-2/3) * 10)**2))
+data2=np.array(data2)
+
+plt.plot(data,label="data1")
+
+plt.xlabel('Index')
+plt.ylabel('Values')
+
+plt.plot(data2,label="data2")
+plt.xlabel('Index')
+plt.ylabel('Values')
+
+plt.show()
